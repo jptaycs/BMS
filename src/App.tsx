307a5bf9945a228a9d7e6d1e0f2c06ac79c270c9
Xaidel <abechuelak@gmail.com>
2025-06-23 08:46:58 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <div className="flex justify-center items-center w-screen h-screen">
+    <Button>Press</Button>
+  </div>
+}
+
+export default App;
