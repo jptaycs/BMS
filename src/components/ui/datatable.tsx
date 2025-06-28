c4aae94d985864d647540938cbb391989cc618e2
Xaidel <abechuelak@gmail.com>
2025-06-28 17:12:29 +0800

    feat: table

---
+++ src/components/ui/datatable.tsx
@@ -0,0 +1,64 @@
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./table"
+type TableProps<T> = {
+  columns: ColumnDef<T>[]
+  data: T[]
+}
+export default function DataTable<T>({ columns, data }: TableProps<T>) {
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <div className="w-full my-10">
+      <Table>
+        <TableHeader >
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}  >
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id} className="text-black font-redhat font-bold py-5">
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id} className="py-5">
+                    {flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext()
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={columns.length}
+                className="h-24 text-center"
+              >
+                No results
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
