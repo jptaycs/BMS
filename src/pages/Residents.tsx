ff34359af09a17d937625537987d2a9b3d9f50a8
jptaycs <jptayco1109@gmail.com>
2025-07-03 18:52:46 +0800

    Residents table
    Created residents' table - unfinish

--- src/pages/Residents.tsx
+++ src/pages/Residents.tsx
@@ -1,5 +1,172 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import DataTable from "@/components/ui/datatable";
+import Filter from "@/components/ui/filter";
+import Searchbar from "@/components/ui/searchbar";
+import AddResidentModal from "@/features/residents/addResidentModal";
+import DeleteResidentModal from "@/features/residents/deleteResidentModal";
+import ViewResidentModal from "@/features/residents/viewResidentModal";
+import { ColumnDef } from "@tanstack/react-table";
+import { format } from "date-fns";
+import { Trash } from "lucide-react";
+
+const filters = [
+  "All Residents",
+  "Alphabetical",
+  "Civil Status",
+  "Age",
+  "Gender",
+  "Zone",
+  "Moved Out",
+  "Active",
+]
+
+type Resident = {
+  fullName: string,
+  civilStatus: string,
+  status: "Moved Out" | "Active" | "Dead" | "Missing",
+  birthday: Date,
+  gender: string,
+  zone: string,
+}
+
+const columns: ColumnDef<Resident>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ? true : table.getIsSomePageRowsSelected() ? "indeterminate" : false
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+        className="flex items-center justify-center"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+        className="flex items-center justify-center"
+      />
+    ),
+  },
+  {
+    header: "Full Name",
+    accessorKey: "fullName"
+  },
+  {
+    header: "Civil Status",
+    accessorKey: "civilStatus",
+  },
+    {
+    header: "Birthday",
+    accessorKey: "birthdate",
+    cell: ({ row }) => {
+      return (
+        <div>{format(row.original.birthday, "MMMM do, yyyy")}</div>
+      )
+    }
+  },
+    {
+    header: "Gender",
+    accessorKey: "gender"
+  },
+      {
+    header: "Zone",
+    accessorKey: "zone"
+  },
+  {
+    header: "Status",
+    accessorKey: "status",
+    cell: ({ row }) => {
+      const status = row.original.status;
+      let color: string;
+      switch (status) {
+        case "Moved Out": {
+          color = "#00BD29";
+          break;
+        }
+        case "Active": {
+          color = "#000000";
+          break;
+        }
+        case "Dead": {
+          color = "#FFB30F";
+          break;
+        }
+        case "Missing": {
+          color = "#BD0000";
+          break;
+        }
+        default: {
+          color = "#000000";
+        }
+      }
+      return (
+        <div style={{ color: color }}>{status}</div>
+      );
+    }
+  }
+]
+
+
+
+const data: Resident[] = [
+  {
+    fullName: "Karl Abechuela",
+    civilStatus: "Single",
+    birthday: new Date("June 29, 2003"),
+    gender: "Male",
+    zone: "Zone",
+    status: "Active",
+  },
+  {
+  fullName: "Karl Abechuela",
+  civilStatus: "Single",
+  birthday: new Date("June 29, 2003"),
+  gender: "Male",
+  zone: "Zone",
+  status: "Active",
+},
+  {
+  fullName: "Karl Abechuela",
+  civilStatus: "Single",
+  birthday: new Date("June 29, 2003"),
+  gender: "Male",
+  zone: "Zone",
+  status: "Active",
+},
+]
+
 export default function Residents() {
   return (
-    <>This is the residents tab</>
+    <>
+      <div className="flex gap-5 w-full items-center justify-center">
+        <Searchbar placeholder="Search event" classname="flex flex-5" />
+        <Filter filters={filters} initial="All Events" classname="flex-1" />
+        <Button variant="destructive" size="lg" >
+          <Trash />
+          Delete Selected
+        </Button>
+        <AddResidentModal />
+      </div >
+      <DataTable<Resident> data={data} columns={[...columns,
+      {
+        id: "view",
+        header: "",
+        cell: ({ row }) => {
+          const status = row.original.status
+          return (
+            < div className="flex gap-3 ">
+              <ViewResidentModal {...row.original} />
+              {status !== "Dead" && status !== "Moved Out" && <DeleteResidentModal {...row.original} />}
+            </div >
+          )
+        }
+      }
+      ]} />
+    </>
   )
 }
--- src/types/formSchema.ts
+++ src/types/formSchema.ts
@@ -41,3 +41,31 @@ export const eventSchema = z.object({
     message: "Important notes is too long"
   })
 })
+export const residentSchema = z.object({
+  fullName: z.string().min(2, {
+    message: "Event name is too short"
+  }).max(50, {
+    message: "Event name is too long, put other details on the 'details' form"
+  }),
+  civilStatus: z.string().min(2, {
+    message: "Event type is too short"
+  }).max(50, {
+    message: "Event type is too long."
+  }),
+  birthday: z.date({
+    required_error: "Please specify the event date"
+  }),
+  gender: z.string().min(2, {
+    message: "Event venue is too short"
+  }).max(50, {
+    message: "Event venue is too long"
+  }),
+  Zone: z.string().min(2, {
+    message: "Atendee too long"
+  }).max(50, {
+    message: "Event venue is too long"
+  }),
+  notes: z.string().max(1000, {
+    message: "Important notes is too long"
+  })
+})
