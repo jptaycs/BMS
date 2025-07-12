e23aa5920c8f100cecf175979ba95f37dd8f7eb7
Xaidel <abechuelak@gmail.com>
2025-07-12 23:39:03 +0800

    feat: searching functionality resident tab

--- src/components/ui/searchbar.tsx
+++ src/components/ui/searchbar.tsx
@@ -1,15 +1,21 @@
 import { Search } from "lucide-react";
 import { Input } from "./input";
 import { cn } from "@/lib/utils";
+import { ChangeEvent } from "react";
 
 type SearchbarParams = {
   placeholder: string
   classname?: string
+  onChange: (searchTerm: string) => void
 }
 
-export default function Searchbar({ placeholder, classname }: SearchbarParams) {
+export default function Searchbar({ placeholder, classname, onChange }: SearchbarParams) {
   return <div className={cn("relative", classname)}>
     <Input
+      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+        onChange(e.target.value)
+
+      }
       className="min-h-12 border-1 border-black/50 pl-16"
       autoComplete="on"
       placeholder={placeholder}
